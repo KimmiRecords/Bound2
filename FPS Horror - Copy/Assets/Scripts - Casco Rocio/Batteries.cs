e09484a0777e613df5eb0ba2a69ad2495707e0cc
Meilen1 <rocio.casco@davinci.edu.ar>
2022-07-25 02:46:08 -0300

    escena de prueba para inventario

---
+++ FPS Horror - Copy/Assets/Scripts - Casco Rocio/Batteries.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batteries : MonoBehaviour
+{
+    
+
+    public void UseBatteries()
+    {
+        if (tag.Equals("batteries"))
+        {
+            Debug.Log("active las baterias");
+
+        }
+
+    }
+}
